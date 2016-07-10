502aeb846a9554378bb5a39141d39654360c62b6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-10 15:03:58 +0000

    rollback to previous behavior
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.namedfile/trunk@92031 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/namedfile/constraints.py
+++ src/nti/namedfile/constraints.py
@@ -44,9 +44,6 @@ class FileConstraints(object):
 		return result
 
 	def is_mime_type_allowed(self, mime_type=None):
-		if "*/*" in self.allowed_mime_types: # allow all
-			return True
-
 		mime_type = mime_type or getattr(self._v_file, 'contentType', None)
 		mime_type = mime_type.lower() if mime_type else mime_type
 		if (	not mime_type  # No input
