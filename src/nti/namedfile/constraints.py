1e7b086194885bb105f1f6af1cb99f6b89c214c0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-28 01:22:36 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.namedfile/trunk@107589 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/namedfile/constraints.py
+++ src/nti/namedfile/constraints.py
@@ -21,67 +21,70 @@ from nti.mimetype.mimetype import mimeTypeConstraint
 from nti.namedfile.interfaces import INamedFile
 from nti.namedfile.interfaces import IFileConstraints
 
+
 @component.adapter(INamedFile)
 @interface.implementer(IFileConstraints, IContentTypeAware)
 class FileConstraints(object):
 
-	mimeType = mime_type = u'application/vnd.nextthought.namedfileconstraints'
+    mimeType = mime_type = u'application/vnd.nextthought.namedfileconstraints'
 
-	_v_file = None
+    _v_file = None
 
-	max_files = 2
-	max_file_size = None
-	allowed_extensions = ('*',)
-	allowed_mime_types = ("*/*",)
+    max_files = 2
+    max_file_size = None
+    allowed_extensions = ('*',)
+    allowed_mime_types = ("*/*",)
 
-	parameters = {} # IContentTypeAware
+    parameters = {}  # IContentTypeAware
 
-	def __init__(self, context=None):  # make it adpater
-		self._v_file = context
+    def __init__(self, context=None):  # make it adpater
+        self._v_file = context
 
-	def is_file_size_allowed(self, size=None):
-		size = self._v_file.getSize() if self._v_file is not None and size is None else size
-		result = not self.max_file_size or (size is not None and size <= self.max_file_size)
-		return result
+    def is_file_size_allowed(self, size=None):
+        if self._v_file is not None and size is None:
+            size = self._v_file.getSize()
+        result = not self.max_file_size \
+              or (size is not None and size <= self.max_file_size)
+        return result
 
-	def is_mime_type_allowed(self, mime_type=None):
-		mime_type = mime_type or getattr(self._v_file, 'contentType', None)
-		mime_type = mime_type.lower() if mime_type else mime_type
-		if (	not mime_type  # No input
-			or not mimeTypeConstraint(mime_type)  # Invalid
-			or not self.allowed_mime_types):  # Empty list: all excluded
-			return False
+    def is_mime_type_allowed(self, mime_type=None):
+        mime_type = mime_type or getattr(self._v_file, 'contentType', None)
+        mime_type = mime_type.lower() if mime_type else mime_type
+        if (   not mime_type  # No input
+            or not mimeTypeConstraint(mime_type)  # Invalid
+            or not self.allowed_mime_types):  # Empty list: all excluded
+            return False
 
-		major, minor = mime_type.split('/')
-		if major == '*' or minor == '*':
-			return False  # Must be concrete
+        major, minor = mime_type.split('/')
+        if major == '*' or minor == '*':
+            return False  # Must be concrete
 
-		for mt in self.allowed_mime_types:
-			if mt == '*/*':
-				return True  # Total wildcard
+        for mt in self.allowed_mime_types:
+            if mt == '*/*':
+                return True  # Total wildcard
 
-			mt = mt.lower()
-			if mt == mime_type:
-				return True
+            mt = mt.lower()
+            if mt == mime_type:
+                return True
 
-			amajor, aminor = mt.split('/')
-			idx = aminor.find(';')
-			if idx != -1: # ignore params
-				aminor = aminor[0:idx]
+            amajor, aminor = mt.split('/')
+            idx = aminor.find(';')
+            if idx != -1:  # ignore params
+                aminor = aminor[0:idx]
 
-			# Wildcards are only reasonable in the minor part,  e.g., text/*.
-			if aminor == minor or aminor == '*':
-				if major == amajor:
-					return True
-		return False
+            # Wildcards are only reasonable in the minor part,  e.g., text/*.
+            if aminor == minor or aminor == '*':
+                if major == amajor:
+                    return True
+        return False
 
-	def is_filename_allowed(self, filename=None):
-		filename = filename or getattr(self._v_file, 'filename', None)
-		ext = os.path.splitext(filename.lower())[1] if filename else None
-		lowered_exts = (x.lower() for x in self.allowed_extensions or ())
-		result = False
-		if filename:
-			result = 	not self.allowed_extensions \
-					or  '*' in self.allowed_extensions \
-					or	ext in lowered_exts
-		return result
+    def is_filename_allowed(self, filename=None):
+        result = False
+        filename = filename or getattr(self._v_file, 'filename', None)
+        ext = os.path.splitext(filename.lower())[1] if filename else None
+        lowered_exts = (x.lower() for x in self.allowed_extensions or ())
+        if filename:
+            result = not self.allowed_extensions \
+                  or '*' in self.allowed_extensions \
+                  or ext in lowered_exts
+        return result
